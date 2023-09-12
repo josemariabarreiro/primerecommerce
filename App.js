e923d1d4e2a9d7a2f09ba5d2597822236caf2efd
unknown <barreiro.jose@outlook.com>
2023-09-11 22:30:47 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import Home from "./src/screem/Home";
+import Search from "./src/components/Search";
+import Products from "./src/screem/Products";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* <Home /> */}
+      {/* <Search /> */}
+      <Products category="smartphones" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
